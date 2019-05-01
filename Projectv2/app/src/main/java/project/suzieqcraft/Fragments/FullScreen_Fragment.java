49c97e32ea0820fea204626bb4a6d274be94f4a2
Warren-93 <43173167+MWarren93@users.noreply.github.com>
2019-05-01 22:02:50 +0100

    Fullscreen Working

--- Projectv2/app/src/main/java/project/suzieqcraft/Fragments/FullScreen_Fragment.java
+++ Projectv2/app/src/main/java/project/suzieqcraft/Fragments/FullScreen_Fragment.java
@@ -3,6 +3,8 @@ package project.suzieqcraft.Fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -24,17 +26,20 @@ public class FullScreen_Fragment extends Fragment {
     RecyclerView fullscreenViewer;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        fullscreenViewer = view.findViewById(R.id.fullscreenViewer);
         fullscreenAdapter = new FullscreenAdapter( imageList );
         fullscreenViewer.setAdapter( fullscreenAdapter );
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate( R.layout.fullscreenfrag, container, false);
 
     }
-
 
     public static FullScreen_Fragment createIntent(ArrayList<Image> productList) {
         FullScreen_Fragment fullScreen_fragment = new FullScreen_Fragment();
